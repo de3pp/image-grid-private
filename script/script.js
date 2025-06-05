0bedcdaeb0933d28544b174864d339077d4d1d9a
de3pp <maxim.kozachenko1234@gmail.com>
2025-06-05 18:09:54 +0200

    Add theme toggle functionality and update footer design
    - Implemented light and dark theme toggles in script.js.
    - Updated footer in index.html to include theme icons and current date.
    - Enhanced responsive styles in responsive.css and style.css for better layout and theme support.

--- script/script.js
+++ script/script.js
@@ -23,4 +23,34 @@ window.addEventListener('load', () => {
     });
   });
 
+  const dateElement = document.getElementById('footer-date');
+
+  if (dateElement) {
+    setInterval(() => {
+      const dateTime = new Date();
+      dateElement.dateTime = dateTime.toISOString();
+      dateElement.textContent = dateTime.toLocaleDateString() + ' ' + dateTime.toLocaleTimeString();
+    }, 500);
+  };
+
+
+  const lightThemeToggle = document.getElementById('light');
+
+  lightThemeToggle.addEventListener('click', () => {
+    document.documentElement.setAttribute('color-scheme', 'light');
+    localStorage.setItem('theme', 'light');
+
+  });
+
+  const darkThemeToggle = document.getElementById('dark');
+
+  darkThemeToggle.addEventListener('click', () => {
+    document.documentElement.setAttribute('color-scheme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  });
+
+  if (localStorage.getItem('theme')) {
+    document.documentElement.setAttribute('color-scheme', localStorage.getItem('theme'));
+  }
 });
+
